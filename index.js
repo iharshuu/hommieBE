d6f058c4d86763dfb743de30c547a3f5f956b438
harshu <harshalmten@gmail.com>
2025-03-16 23:17:06 +0530

    updated cors

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 
 mongoose.connect(`mongodb+srv://harshalmten:${process.env.mangoPassword}@cluster0.awdcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => console.log('Connected to database!'));
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }));
 app.get("/",(req,res)=>{
